182fff2a7838ce2d901e07cbf0114e1902b87268
SHREYANSH <137887705+02shreyansh@users.noreply.github.com>
2025-07-05 17:01:18 +0000

    get

--- patient-service/patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -1,10 +1,13 @@
 package com.pm.patientservice.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.service.PatientService;
 
 @RestController
@@ -17,6 +20,7 @@ public class PatientController {
 
     @GetMapping
     public ResponseEntity<List<PatientResponseDTO>> getPaients(){
-
+        List<PatientResponseDTO> patients=patientService.getPatients();
+        return ResponseEntity.ok().body(patients);
     }
 }
